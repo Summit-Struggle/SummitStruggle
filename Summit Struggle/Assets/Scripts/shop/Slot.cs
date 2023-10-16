0e89f3d8c580ff0058c8bb5c7aaa5800f4d3a2e2
Jade Thompson-Tavai <jhp4379@autuni.ac.nz>
2023-10-16 15:45:21 +1300

    Update Player Movement as Enum

--- Summit Struggle/Assets/Scripts/shop/Slot.cs
+++ Summit Struggle/Assets/Scripts/shop/Slot.cs
@@ -1,31 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using TMPro;
 
-public class Slot : MonoBehaviour
-{
+// public class Slot : MonoBehaviour
+// {
 
-    [SerializeField] public Image itemImage;
-    [SerializeField] public TextMeshProUGUI itemName;
-    [SerializeField] public TextMeshProUGUI itemPrice;
-    [SerializeField] public TextMeshProUGUI itemAmount;
+//     [SerializeField] public Image itemImage;
+//     [SerializeField] public TextMeshProUGUI itemName;
+//     [SerializeField] public TextMeshProUGUI itemPrice;
+//     [SerializeField] public TextMeshProUGUI itemAmount;
 
-    [SerializeField] public GameObject itemToBuy;
-    [SerializeField] public int _ItemAmount;
-    [SerializeField] public TextMeshProUGUI buyPriceText;
+//     [SerializeField] public GameObject itemToBuy;
+//     [SerializeField] public int _ItemAmount;
+//     [SerializeField] public TextMeshProUGUI buyPriceText;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // itemName.text = itemToBuy.GetComponent<>().itemName;
-    }
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         // itemName.text = itemToBuy.GetComponent<>().itemName;
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
-}
+//     }
+// }
