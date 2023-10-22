42d891c8862617940097f5b5676db3e807af7399
Jade Thompson-Tavai <jhp4379@autuni.ac.nz>
2023-10-22 21:02:58 +1300

    Added coins sprite + currency

--- Summit Struggle/Assets/Currency.cs
+++ Summit Struggle/Assets/Currency.cs
@@ -1,29 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
+
 
 public class Currency : MonoBehaviour
 {
     //set initial currency
-    private int currency = 0;
-    [SerializeField] private TextMeshPro coins;
+    private int currency;
+    [SerializeField] private Text coins;
 
     // Start is called before the first frame update
     void Start()
     {
-        currency = 20;
+        currency = 0;
     }
 
 
     //update UI
-    private void SetCurrency(int currency) { 
-        coins.text = currency;
+    private void SetCurrency() { 
+        string currencyText = currency.ToString();
+        coins.text = currencyText;
     }
 
-    //Buy
+   private void gainCoins(int amount) {
+        currency += amount;
+        SetCurrency();
+    }
 
-
+      private void loseCoins(int amount) {
+        currency -= amount;
+        SetCurrency();
+    }
 
 
     //can set tests for currency increasing when enemy dies
