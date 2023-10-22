79f063fd7e0b5dac13cd537798a323b8fae3e141
CalebSimmons1 <xcz0684@autuni.ac.nz>
2023-10-22 14:29:27 +1300

    added to the UI
    added and started to link text, as whilst as making up a calculation to determine what the players rewards will be, next up will be to get the text to update to display those benefits as I am having a few issues currently

--- Summit Struggle/Assets/Scripts/LevelingBenefits.cs
+++ Summit Struggle/Assets/Scripts/LevelingBenefits.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelingBenefits : MonoBehaviour
 {
-    
+    public PlayerLevel playerLevel;
+    public Text level;
+    public Text rewards;
+  
+    private void Awake()
+    {
+        playerLevel = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerLevel>(); //initializes the PlayerLevel to make sure its correct
+    }
     public GameObject Header;
     void Update()
     {
@@ -12,12 +20,22 @@ public class LevelingBenefits : MonoBehaviour
         {
             ToggleHeaderVisibility();
         }
+
+        level.text = "Level: " + playerLevel.level;
+        
     }
 
     private void ToggleHeaderVisibility()
     {
         // Toggle the active state of the header
         Header.SetActive(!Header.activeSelf);
+    }
+
+    private void BenefitCalculation()
+    {
+        int roundedLevel = Mathf.CeilToInt(playerLevel.level); // Round up the level so its calculating the rewards for the next level
+        float coins = 5 * roundedLevel;
+        rewards.text = "Rewards for next level are: " + coins + "coins";
     }
 }
 
