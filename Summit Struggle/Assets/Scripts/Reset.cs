d42560ff57e0817fa688055a9ccc5c4a66f19537
CalebSimmons1 <xcz0684@autuni.ac.nz>
2023-10-24 21:42:02 +1300

    updated to public

--- Summit Struggle/Assets/Scripts/Reset.cs
+++ Summit Struggle/Assets/Scripts/Reset.cs
@@ -6,12 +6,12 @@ public class Reset : MonoBehaviour
 {
     public PlayerLife playerLife;
 
-    void Start()
+    public void Start()
     { 
         playerLife = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerLife>(); //initializes the PlayerLife so the ResetGame method can be called to rest the level
     }
 
-    void Update()
+    public void Update()
     {
         if (Input.GetKeyDown(KeyCode.R)) //checks for the users input of R key and resets the game if it is pressed
         {
